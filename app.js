213457e02d71b2743ea60a803692f1bae77210ab
Anindya Ray <anindyaray44@gmail.com>
2021-05-03 15:10:09 +0530

    added the sorting visual

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 
 let body=document.querySelector('.container');
+let input=[10,12,4,56,10,23,15,16,11,2,4,5,19,29,30,44,1,2,9,32,33,21,44,31];
+let inputnum= input.length;
 
 
-
-function display(input,inputnum){
+function display(){
     console.log(input)
     for(let num=0;num<inputnum;num++){
         let height=input[num]*10;
@@ -16,21 +17,29 @@ function display(input,inputnum){
 }
 
 
-function bubbleSort(input,inputnum)
+async function bubbleSort()
 {
-   for (let i = 0; i < inputnum-1; i++){ 
-       for (let j = 0; j < inputnum-1; j++){
-           if (input[j] > input[j+1])
-                {
-                    let temp=input[j];
-                    input[j]=input[j+1];
-                    input[j+1]=temp;
-                }
-       }
+   for (let i = 0; i < inputnum-1; i++){
+      display();
+     
+      for (let j = 0; j < inputnum-1; j++){
+        if (input[j] > input[j+1])
+             {
+                 let temp=input[j];
+                 input[j]=input[j+1];
+                 input[j+1]=temp;
+             }
     }
+    await new Promise((resolve) =>
+        setTimeout(() => {
+          resolve();
+        }, 300)
+    );
+    remove();
+} 
 }
 
-function remove(input,inputnum){
+function remove(){
     const elements= body.querySelectorAll('.heights');
    for(let ele of elements){
        ele.remove();
@@ -39,12 +48,11 @@ function remove(input,inputnum){
 
 
 function main(){
-    let input=[10,12,4,56,10,23,15,16,11,2,4,5,19,29,30,44,1,2,9,32,33,21,44,31];
-    let inputnum= input.length;
-    display(input,inputnum);
-    bubbleSort(input,inputnum);
-    remove(input,inputnum);
-    display(input,inputnum);
+  
+    // display();
+    bubbleSort();
+    setTimeout(display,300*inputnum);
+   
 
 
 
