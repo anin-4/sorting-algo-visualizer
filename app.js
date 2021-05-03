17168223588daf4ef8b2a9962b03b86a0b402050
Anindya Ray <anindyaray44@gmail.com>
2021-05-03 14:30:32 +0530

    almost getting bubble sort to work

--- app.js
+++ app.js
@@ -1,14 +1,55 @@
-let input=[10,12,14,16,18,20,22,24,26,28,30,32,34,36,38,40,42,44,46,48,50,52,54,56,58];
 
 let body=document.querySelector('.container');
 
-let inputnum= input.length;
 
-for(let num=0;num<inputnum;num++){
-    let height=input[num]*10;
-    let element=document.createElement('div');
-    body.append(element);
-    element.setAttribute("class","heights");
-    element.style.height=height+"px";
-    
+
+function display(input,inputnum){
+    console.log(input)
+    for(let num=0;num<inputnum;num++){
+        let height=input[num]*10;
+        let element=document.createElement('div');
+        body.append(element);
+        element.setAttribute("class","heights");
+        element.style.height=height+"px";
+        
+    }
 }
+
+
+function bubbleSort(input,inputnum)
+{
+   for (let i = 0; i < inputnum-1; i++){ 
+       for (let j = 0; j < inputnum-1; j++){
+           if (input[j] > input[j+1])
+                {
+                    let temp=input[j];
+                    input[j]=input[j+1];
+                    input[j+1]=temp;
+                }
+       }
+    }
+}
+
+function remove(input,inputnum){
+    const elements= body.querySelectorAll('.heights');
+   for(let ele of elements){
+       ele.remove();
+   }
+}
+
+
+function main(){
+    let input=[10,12,4,56,10,23,15,16,11,2,4,5,19,29,30,44,1,2,9,32,33,21,44,31];
+    let inputnum= input.length;
+    display(input,inputnum);
+    bubbleSort(input,inputnum);
+    remove(input,inputnum);
+    display(input,inputnum);
+
+
+
+
+}
+main();
+
+
